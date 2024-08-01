5dac9b593c95557fbdf164527464c457dbab4fab
Draweshwarkumar <shivamjha22948@gmail.com>
2024-08-01 17:58:29 +0530

    iteration in thymeleaf

--- src/main/java/com/practice/controller/MyController.java
+++ src/main/java/com/practice/controller/MyController.java
@@ -1,11 +1,12 @@
 package com.practice.controller;
 
-import java.sql.Date;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,15 +19,27 @@ public class MyController {
 
         model.addAttribute("name", "Shivam Jha");
 
-//        // Get current date and time and format it
-//        LocalDateTime now = LocalDateTime.now();
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-//        String formattedDate = now.format(formatter);
+        // Get current date and time using LocalDateTime and format it
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedDate = now.format(formatter);
 
+        model.addAttribute("currentDate", formattedDate);
 
         return "about";
     }
+    
+    @GetMapping("/example-loop")
+    public String iterateHandler(Model m) {
+    	
+    	List<String> names = List.of("shivam","ritik","rishav","pankaj");
+    	
+    	m.addAttribute("names",names);
+    	
+    	return "Iterate";
+    }
 }
+
 
 
  
